5a503a82b6e98f2987b0ea1d050d47d2f28a5259
YoungHoonYI <hoon5554@naver.com>
2022-01-14 11:24:54 +0900

    ㅇㅇㅇ
    ㅇㅇㅇ

--- Spring_final/src/main/java/com/spring/model/Review/A_ReviewDAO.java
+++ Spring_final/src/main/java/com/spring/model/Review/A_ReviewDAO.java
@@ -2,11 +2,14 @@ package com.spring.model.Review;
 
 import java.util.List;
 
+
+
 public interface A_ReviewDAO {
 	
 	List<A_ReviewDTO> ARList(String str);
 	int insertAR(A_ReviewDTO dto);
 	int updateAR(A_ReviewDTO dto);
 	int deleteAR(int no);
+
 	
 }
